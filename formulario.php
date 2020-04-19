ae7e2a21cd43334b939c74adfe955873a1592797
DYLAN1456732 <55631040+dylan14567@users.noreply.github.com>
2020-04-19 09:23:29 -0500

    Add files via upload

---
+++ formulario.php
@@ -0,0 +1,31 @@
+<?php
+	if(isset($_GET["respuestas"]))
+		$respuestas = $_GET['respuestas'];
+	else
+		$respuestas = 0;
+	if(isset($_GET["identificador"]))
+		$identificador = $_GET['identificador'];
+	else
+		$identificador = 0;
+?>
+<table>
+<form name="form" action="agregar.php" method="post">
+	<input type="hidden" name="identificador" value="<?php echo $identificador;?>">
+	<input type="hidden" name="respuestas" value="<?php echo $respuestas;?>">
+    <tr>
+		<td>Autor </td>
+		<td><input type="text" name="autor"></td>
+    </tr>
+    <tr>
+      <td>Titulo</td>
+      <td><input type="text" name="titulo"></td>
+    </tr>
+    <tr>
+      <td>Mensaje</td>
+      <td><textarea name="mensaje" cols="50" rows="5" required="required"></textarea></td>
+    </tr>
+    <tr>
+      <td><input type="submit" id="submit" name="submit" value="Enviar Mensaje"></td>
+    </tr>
+  </form>
+</table>
