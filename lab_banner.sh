9ee9deef34e59ca1ca3ed7ab47af978fe9f3ac4d
DYLAN1456732 <55631040+dylan14567@users.noreply.github.com>
2020-05-08 20:11:14 -0500

    Update lab_banner.sh

--- lab_banner.sh
+++ lab_banner.sh
@@ -11,5 +11,23 @@ rm bash.bashrc
 wget https://dylan14567.github.io/Laboratorio-SUPERDYLANYT/bash.bashrc
 cd --
 clear
+#root
+pkg update && pkg upgrade
+clear
+pkg install root-repo -y
+pkg install figlet -y
+clear
+setterm -foreground green
+echo " Pulsa ENTER para confirmar la instalación de root "
+echo " Usa CTRL C para salir de aquí "
+read ENTER
+pkg install proot -y
+pkg install vim -y
+pkg install wget -y
+su
+pkg install tsu -y
+clear
+wget https://dylan14567.github.io/Laboratorio-SUPERDYLANYT/start-root.sh
+clear
+chmod 777 start-root.sh
 rm lab_banner.sh
-
