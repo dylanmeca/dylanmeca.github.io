50d12c9dfd57de28662e5a0429d6e42cd57a75dc
DYLAN1456732 <55631040+dylan14567@users.noreply.github.com>
2020-05-15 18:29:38 -0500

    Update lab_tool.sh

--- lab_tool.sh
+++ lab_tool.sh
@@ -10,7 +10,7 @@ echo -e "\n\e[5;91m   1- Instalar VPN"
 echo -e "\e[5;96m   2- Instalar GENERADOR DE PAYLOAD Basico"
 echo -e "\e[5;92m   3- Instalar nmap"
 echo -e "\e[5;92m   4- Instalar Hydra"
-echo -e "\e[5;92m   5- Salir"
+echo -e "\e[5;92m   5- Instalar IPGeolocation"
 echo -e -n "\e[5;92m \n >>> "
 read res
 case $res in
@@ -90,6 +90,28 @@ exit
 fi
 ;;
 "5")
+clear
+pkg update && pkg upgrade
+pkg install fish git python
+git clone https://github.com/maldevel/IPGeoLocation
+pip install --upgrade pip
+cd IPGeoLocation;ls
+pip install -r requirements.txt
+chmod 777 ipgeolocation.py
+python ipgeolocation.py
+ls
+clear
+python ipgeolocation.py -m
+echo -e "\e[5;96m Regresar al menu s/n?"
+read s
+if [ "$s" = "s" ]; then 
+menu
+else
+echo "saliendo.."
+exit
+fi
+;;
+"7")
 exit
 ;;
 esac
