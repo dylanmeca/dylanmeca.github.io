79784d936553ccb244d7a0c5b72c0161c12c32cc
DYLAN1456732 <55631040+dylan14567@users.noreply.github.com>
2020-11-03 18:59:34 -0500

    Update app2.js

--- Server/js/app2.js
+++ Server/js/app2.js
@@ -1,21 +1,4 @@
 Swal.fire({
   title: 'Login Form',
-  html: `<input type="text" id="login" class="swal2-input" placeholder="Username">
-  <input type="password" id="password" class="swal2-input" placeholder="Password">`,
-  showCancelButton: false,
-  confirmButtonText: 'Sign in',
-  focusConfirm: false,
-  preConfirm: () => {
-    const login = Swal.getPopup().querySelector('root').value
-    const password = Swal.getPopup().querySelector('root').value
-    if (!login || !password) {
-      Swal.showValidationMessage(`Please enter login and password`)
-    }
-    return { login: login, password: password }
-  }
-}).then((result) => {
-  Swal.fire(`
-    Login: ${result.value.login}
-    Password: ${result.value.password}
-  `.trim())
+  password: true
 })
