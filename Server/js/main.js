7797d717cca9b1d5ebd6d763beae156a9fc516a4
dylan14567 <mundodylan14567dylanmotta78@gmail.com>
2021-03-08 10:25:46 -0500

    Nueva Actualizacion

--- Server/js/main.js
+++
@@ -1,6 +0,0 @@
-const btnSwitch = document.querySelector('#switch');
-
-btnSwitch.addEventListener('click', () => {
-	document.body.classList.toggle('dark');
-	btnSwitch.classList.toggle('active');
-});
